2e2227e77718a1cdd9db71b9ed69b3ee14574f6f
Darkord2028 <98699507+Darkord2028@users.noreply.github.com>
2025-06-23 18:28:33 +0530

    I am done

--- MeleeMayhem/Assets/Scripts/Enemy/Enemy.cs
+++ MeleeMayhem/Assets/Scripts/Enemy/Enemy.cs
@@ -1,10 +1,14 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour, IDamagable
 {
-    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] float maxHealth = 100f;
     [SerializeField] float currentHealth;
+    [SerializeField] float viewRadius;
+    [SerializeField] Vector3 sphereOffset;
+    [SerializeField] LayerMask playerMask;
 
     public float CurrentHealth => currentHealth;
     public float MaxHealth => maxHealth;
@@ -14,15 +18,66 @@ public class Enemy : MonoBehaviour, IDamagable
 
     private bool isDead = false;
 
+    private Transform player;
+    private Animator animator;
+
+    private Collider[] hitResults = new Collider[1];
+
+    #region Unity Callback Function
     private void Awake()
     {
         currentHealth = maxHealth;
     }
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        player = CheckForPlayer();
+        if (player != null)
+        {
+            transform.LookAt(player);
+        }
+    }
+
+    #endregion
+
+    #region Check Functions
+
+    private Transform CheckForPlayer()
+    {
+        Transform playerTransform = null;
+        float closestDistanceSqr = Mathf.Infinity;
+
+        int hitCount = Physics.OverlapSphereNonAlloc(transform.position + sphereOffset, viewRadius, hitResults, playerMask);
+
+        for (int i = 0; i < hitCount; i++)
+        {
+            Transform target = hitResults[i].transform;
+            float distToTarget = Vector3.Distance(transform.position, target.position);
+
+            if (distToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = distToTarget;
+                playerTransform = target;
+            }
+        }
+
+        return playerTransform;
+    }
+
+    #endregion
+
+    #region Enemy Damage
+
     public void TakeDamage(float amount)
     {
         if (isDead) return;
 
+        animator.SetBool("hit", true);
         currentHealth -= amount;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         OnTakeDamage?.Invoke(amount);
@@ -30,6 +85,7 @@ public class Enemy : MonoBehaviour, IDamagable
         if (currentHealth <= 0)
         {
             Die();
+            animator.SetBool("hit", false);
         }
     }
 
@@ -37,6 +93,20 @@ public class Enemy : MonoBehaviour, IDamagable
     {
         isDead = true;
         OnDeath?.Invoke(this.gameObject);
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
+        animator.SetBool("die", true);
     }
+
+    #endregion
+
+    #region Gizmos
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position + sphereOffset, viewRadius);
+    }
+
+    #endregion
+
 }
